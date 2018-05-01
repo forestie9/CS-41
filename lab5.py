a4f8744440dd7a1142e80c540785bf109d45032a
forestie9 <forestie9@users.noreply.github.com>
2018-05-01 11:16:15 -0500

    Last three lasb

---
+++ lab5.py
@@ -0,0 +1,14 @@
+# Lab 6: Standard Library
+# 5/1/2018 Created
+
+# collections
+# collections.namedtuple
+
+# Regular expressions
+# Regex Crossword Checker
+
+# Itertools
+# random
+# sys
+
+# Lab 7: 3rd Party Library
